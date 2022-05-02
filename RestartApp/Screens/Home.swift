8f2ef91631db5f3b463e3860b959beb0d1022ee3
Adam Januszewski <Adpjan@att.net>
2022-05-02 16:58:25 -0400

    first commit

---
+++ RestartApp/Screens/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  RestartApp
+//
+//  Created by adam janusewski on 5/2/22.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
---
+++ RestartApp/Views/CircleGroupView.swift
@@ -0,0 +1,20 @@
+//
+//  CircleGroupView.swift
+//  RestartApp
+//
+//  Created by adam janusewski on 5/2/22.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleGroupView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleGroupView()
+    }
+}
