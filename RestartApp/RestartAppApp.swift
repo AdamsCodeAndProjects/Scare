f664914940001099e5132bb8a16cbabd8bbce60b
Adam Januszewski <Adpjan@att.net>
2022-05-02 11:42:15 -0400

    Initial Commit

---
+++ RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by adam janusewski on 5/2/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
